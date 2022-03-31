ca7abea4bcad0f16f807bf2ad33de958b8d97e09
DavidHernandez21 <55497518+DavidHernandez21@users.noreply.github.com>
2022-03-31 09:17:03 +0200

    Add files via upload

---
+++ ArjanCodes/brigde_pattern/with_abc/stream/service.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+
+from stream import data
+
+
+@dataclass
+class StreamingService(ABC):
+
+    devices: list[data.Buffer] = field(default_factory=list)
+
+    def add_device(self, device: data.Buffer) -> None:
+        self.devices.append(device)
+
+    def retrieve_buffer_data(self) -> list[data.BufferData]:
+        return [device() for device in self.devices]
+
+    @abstractmethod
+    def start_stream(self) -> str:
+        pass
+
+    @abstractmethod
+    def fill_buffer(self, stream_reference: str) -> None:
+        pass
+
+    @abstractmethod
+    def stop_stream(self, stream_reference: str) -> None:
+        pass
