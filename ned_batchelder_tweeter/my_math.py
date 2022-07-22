052362d9c338ebf7f9f02f712e46a4d8282109de
David Hernandez <asbeldavid.hernandez@gmail.com>
2022-07-22 14:56:09 +0200

    added ned_batchelder_tweeter folder

---
+++ ned_batchelder_tweeter/my_math.py
@@ -0,0 +1,8 @@
+import auto_math
+
+print(auto_math.plus_10(5))
+print(auto_math.times_10(5))
+print(auto_math.divide_4_5(4.5))
+# print(auto_math.power_(2.0))
+print(auto_math.__getattr__('plus_10_5')(5))
+# print(auto_math.minus_5_daje(89))
