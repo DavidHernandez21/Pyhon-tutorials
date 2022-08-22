663676ea449499c2f97546307d2f1bcd9de3621a
David Hernandez <asbeldavid.hernandez@gmail.com>
2022-08-22 10:21:15 +0200

    added flower config and monitoring

--- celery/celery_worker/main.py
+++ celery/celery_worker/main.py
@@ -1,24 +1,29 @@
+import concurrent.futures
+from time import perf_counter_ns
+
 from tasks import add
 
 # import time
 
 
-def main():
+def work(timeout: int) -> str:
     result = add.delay(2, 2)
-    print(result.ready())
-    # print(dir(result))
-    print(result.status)
-    print(result.state)
-    print(result.successful())
+    # print(f"result is ready: {result.ready()}")
+    # # print(dir(result))
+    # print(result.status)
+    # print(result.state)
+    # print(result.successful())
     # print(result.backend)
     # start = time.perf_counter()
     # print(result.wait())
-    print(result.get(timeout=5))
-    print(result.ready())
+    # print(result.get(timeout=timeout))
+    # print(f"result is ready: {result.ready()}")
     # print(dir(result))
-    print(result.status)
-    print(result.state)
-    print(result.successful())
+    # print(result.status)
+    # print(result.state)
+    # print(result.successful())
+    value = result.get(timeout=timeout)
+    return f'result state: {result.state}, result was succesful: {result.successful()}, result value: {value}'
     # print(time.perf_counter() - start)
     # r = app.AsyncResult("240407a3-d992-4403-a2a3-469dddcf5b74")
     # print(dir(app.AsyncResult("240407a3-d992-4403-a2a3-469dddcf5b74")))
@@ -34,5 +39,35 @@ def main():
     # print(r.get())
 
 
+def concurrent_map():
+    timeouts = [10 for _ in range(30)]
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        result = executor.map(work, timeouts, timeout=20)
+        # print(result.result())
+    for v in result:
+        print(v)
+
+
+def concurrent_submit():
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = (executor.submit(work, 10) for _ in range(30))
+
+        for future in concurrent.futures.as_completed(results, timeout=20):
+            try:
+                # result =
+                print(future.result())
+            except concurrent.futures.TimeoutError as e:
+                print(e)
+
+
+def main():
+    start = perf_counter_ns()
+    # concurrent_submit()
+    concurrent_map()
+
+    print(f'process took {(perf_counter_ns() - start) * 10e-10:.2f}s')
+
+
 if __name__ == '__main__':
+
     main()
---
+++ celery/flower_conf/flowerconfig.py
@@ -0,0 +1,3 @@
+# RabbitMQ management api
+broker_api = 'amqp://guest:guest@rabbitmq:5672'
+auth_provider = 'flower.views.auth.GithubLoginHandler'
