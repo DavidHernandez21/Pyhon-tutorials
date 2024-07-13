5e8da15e4f1c065d2b8cfa9a6b11a56dc37e3a97
David Hernandez <asbeldavid.hernandez@gmail.com>
2024-07-13 18:33:31 +0200

    Ruff linter

--- iterator_out_of_a_function/main.py
+++ iterator_out_of_a_function/main.py
@@ -6,31 +6,29 @@ from socket import socket
 
 def roll_dice_simulator(dice_max_value: int = 6) -> Generator[int, None, None]:
     for die_roll in iter(lambda: randint(1, dice_max_value), dice_max_value):
-        print(f'handling simulation: {die_roll}')
+        print(f"handling simulation: {die_roll}")
         yield die_roll
 
 
-def from_input(exit_string: str = 'exit') -> Generator[str, None, None]:
-    for user_str in iter(input, exit_string):
-        yield user_str
+def from_input(exit_string: str = "exit") -> Generator[str, None, None]:
+    yield from iter(input, exit_string)
 
 
 def chunk_reader(chunk_size: int = 1024) -> Generator[bytes, None, None]:
-    for block in iter(partial(socket.recv, chunk_size), b''):
-        yield block
+    yield from iter(partial(socket.recv, chunk_size), b"")
 
 
 def main():
-    exit_string = 'exit'
+    exit_string = "exit"
     print(f"Type '{exit_string}' to exit the program.")
     for user_str in from_input(exit_string=exit_string):
-        print(f'User typed {user_str!r}.')
+        print(f"User typed {user_str!r}.")
 
     dice_max_value = 6
-    print(f'Simulation will run until random sample {dice_max_value} is thrown.')
+    print(f"Simulation will run until random sample {dice_max_value} is thrown.")
     for die_roll in roll_dice_simulator(dice_max_value=dice_max_value):
-        print()
+        print(f"Random sample: {die_roll}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
